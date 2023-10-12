0d9cb16c8875f36fc178969b870ffee084c20802
Actual-PT <147751603+Actual-PT@users.noreply.github.com>
2023-10-12 11:26:27 -0700

    Add files via upload
    Most of the files minus bootstrap

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelector('#spotify-btn').addEventListener('click',function(){
+    document.location.href='https://open.spotify.com/show/1NTqlvdt6QYu0WdUY6Rsmp?si=5bd4c6be97d44791';
+});
